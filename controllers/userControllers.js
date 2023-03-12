6eb38457c0bfaca8055926d51d3efe662a3625fc
Anna Perlack <annaperlack@gmail.com>
2023-03-12 14:45:21 -0400

    completed user controllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,4 @@
-const { User, Thought } = require('../models');
+const { User} = require('../models');
 
 module.exports = {
     getUsers(req, res) {
@@ -7,5 +7,48 @@ module.exports = {
           .catch((err) => res.status(500).json(err));
       },
 
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) => {
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
 
